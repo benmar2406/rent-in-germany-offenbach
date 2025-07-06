a39e0042993eed1e76ef552ac6a35ad3ae7f35a8
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 18:52:02 +0200

    upd

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// Remove base path for development, use /mieten for production
 const dev = process.argv.includes('dev');
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-		}),
-		paths: {
-			base: dev ? '' : '/rent-in-germany-offenbach'
-		}
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    paths: {
+      base: dev ? '' : '/rent-in-germany-offenbach'
+    },
+    appDir: '_app'
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
