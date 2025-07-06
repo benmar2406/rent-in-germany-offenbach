d02661dd1f0c841b2f72826d1f4eb87fe4807316
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 18:52:32 +0200

    Updates

--- _app/immutable/chunks/B8WG3gTK.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1dtfm2l)==null?void 0:s.base)??"/rent-in-germany-offenbach";var e;const a=((e=globalThis.__sveltekit_1dtfm2l)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/kQzzay2D.js
@@ -0,0 +1,1 @@
+var e;const a=((e=globalThis.__sveltekit_1e74dne)==null?void 0:e.base)??"/rent-in-germany-offenbach";var s;const t=((s=globalThis.__sveltekit_1e74dne)==null?void 0:s.assets)??a;export{t as a,a as b};
