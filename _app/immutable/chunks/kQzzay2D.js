8dc28fcf79c9ad9de9f0f8ad77cccbd2024bb0ca
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 18:53:40 +0200

    Updates

--- _app/immutable/chunks/kQzzay2D.js
+++
@@ -1,1 +0,0 @@
-var e;const a=((e=globalThis.__sveltekit_1e74dne)==null?void 0:e.base)??"/rent-in-germany-offenbach";var s;const t=((s=globalThis.__sveltekit_1e74dne)==null?void 0:s.assets)??a;export{t as a,a as b};
