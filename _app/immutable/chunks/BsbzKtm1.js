30931684568033afcb43f4778edbdf62cbb1ed11
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 17:49:23 +0200

    Updates

--- _app/immutable/chunks/BsbzKtm1.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_183dgeo)==null?void 0:s.base)??"/rent-in-germany-offenbach";var e;const t=((e=globalThis.__sveltekit_183dgeo)==null?void 0:e.assets)??a;export{t as a,a as b};
