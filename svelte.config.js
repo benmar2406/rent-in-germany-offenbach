361a74d64fb03d38c7e01a3749e35e42b6a3f30f
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 18:38:32 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,7 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
 		}),
 		paths: {
 			base: dev ? '' : '/rent-in-germany-offenbach'
