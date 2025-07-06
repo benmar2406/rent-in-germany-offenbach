307cf826c13346012b95062639a9fe357f91f179
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 18:28:21 +0200

    upd

--- build/_app/immutable/chunks/B_jNnQ6a.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./S6aSSAVT.js";a();
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()]
+};
+
+export default config;
