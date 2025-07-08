7db7a0fcfe50f434fd53e5a41a70de9530e832d2
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-08 19:44:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  server: {
-    host: true,
-    allowedHosts: ['devserver-preview--mieten-offenbach.netlify.app']
-  }
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+        server: {
+          host: true,
+          allowedHosts: ['devserver-preview--mieten-offenbach.netlify.app']
+        }
+      }
+    : {})
 };
 
 export default config;
